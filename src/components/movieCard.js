dc00b7fbb3c5528cb0db79155910b0b0d40dd0d2
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-11-10 15:52:55 +0300

    [ADD] primitive search

---
+++ src/components/movieCard.js
@@ -0,0 +1,214 @@
+const movieTemplate = document.createElement('template');
+
+const movieCardStyles = `
+  .result-list__poster-item {
+    border-radius: 12px;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .result-list__poster-item__link {
+    display: block;
+    height: 100%;
+    width: 100%;
+  }
+
+  .result-list__poster-item__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+  }
+
+  .result-list__poster-item__decore-background {
+    display: none;
+  }
+
+  .result-list__poster-item__description-wrapper {
+    position: absolute;
+    overflow: hidden;
+    max-height: 40%;
+    left: 20px;
+    right: 20px;
+    bottom: 20px;
+    display: none;
+  }
+
+  .result-list__poster-item:hover {
+    cursor: pointer;
+  }
+  
+  .result-list__poster-item:hover .result-list__poster-item__decore-background {
+    display: block;
+    border-radius: 12px;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0) 26.43%,
+      rgba(0, 0, 0, 0.8) 72.41%
+    );
+    backdrop-filter: blur(2px);
+  }
+  
+  .result-list__poster-item:hover .result-list__poster-item__description-wrapper {
+    display: grid;
+    grid-template-columns: minmax(0px, 3fr) minmax(0px, 1fr);
+    grid-template-rows: auto auto auto;
+    grid-template-areas:
+      'rating rating'
+      'title title'
+      'genre year';
+    row-gap: 4px;
+    column-gap: 46px;
+  }
+  
+  .result-list__poster-item__rating-wrapper {
+    grid-area: rating;
+    overflow: hidden;
+    display: flex;
+  }
+  
+  .result-list__poster-item__rating {
+    overflow: hidden;
+    display: flex;
+    align-items: flex-end;
+    margin-left: 4px;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    color: white;
+  }
+  
+  .result-list__poster-item__title {
+    grid-area: title;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    color: white;
+  }
+  
+  .result-list__poster-item__genre {
+    grid-area: genre;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    color: rgba(255, 255, 255, 0.4);
+    margin-top: 12px;
+  }
+  
+  .result-list__poster-item__year {
+    grid-area: year;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    text-align: right;
+    color: rgba(255, 255, 255, 0.4);
+    margin-top: 12px;
+  }
+`;
+
+const movieCardHtml = `
+    <div class="result-list__poster-item">
+        <a href="" class="result-list__poster-item__link">
+            <img class="result-list__poster-item__image" src="" />
+            <div class="result-list__poster-item__decore-background"></div>
+            <div class="result-list__poster-item__description-wrapper">
+                <div class="result-list__poster-item__rating-wrapper">
+                    <img src="" />
+                    <div class="result-list__poster-item__rating"></div>
+                </div>
+                <div class="result-list__poster-item__title"></div>
+                <div class="result-list__poster-item__genre"></div>
+                <div class="result-list__poster-item__year"></div>
+            </div>
+        </a>
+    </div>
+`;
+
+movieTemplate.innerHTML = `
+  <style>
+    ${movieCardStyles}
+  </style>
+
+  ${movieCardHtml}
+`;
+
+const params = ['title', 'poster', 'link', 'year', 'genre', 'rating'];
+
+const mirror = (params, element) => {
+  params.forEach((param) => {
+    Object.defineProperty(element, param, {
+      get() {
+        return this.getAttribute(param);
+      },
+      set(value) {
+        this.setAttribute(param, value);
+      },
+    });
+  });
+};
+
+class MovieCard extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'open' });
+    const template = movieTemplate.content.cloneNode(true);
+
+    shadow.appendChild(template);
+    mirror(params, this);
+  }
+
+  static get observedAttributes() {
+    return params;
+  }
+
+  attributeChangedCallback(param, oldValue, newValue) {
+    switch (param) {
+      case 'title':
+        return (this.shadowRoot.querySelector(
+          '.result-list__poster-item__title'
+        ).textContent = newValue);
+
+      case 'poster':
+        return (this.shadowRoot.querySelector(
+          '.result-list__poster-item__image'
+        ).src = newValue);
+
+      case 'link':
+        return (this.shadowRoot.querySelector(
+          '.result-list__poster-item__link'
+        ).href = newValue);
+
+      case 'year':
+        return (this.shadowRoot.querySelector(
+          '.result-list__poster-item__year'
+        ).textContent = newValue);
+
+      case 'rating':
+        return (this.shadowRoot.querySelector(
+          '.result-list__poster-item__rating'
+        ).textContent = newValue);
+
+      case 'genre':
+        return (this.shadowRoot.querySelector(
+          '.result-list__poster-item__genre'
+        ).textContent = newValue);
+    }
+  }
+}
+
+customElements.define('movie-card', MovieCard);
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { mapMovie } from './helpers/mapMovie.js';
+
+//Components
+import './components/movieCard.js';
+
+const searchStates = {
+  default: 'search-wrapper-default',
+  active: 'search-wrapper-active',
+  scroll: 'search-wrapper-scroll',
+};
+
+const resultStates = {
+  none: 'result-wrapper-none',
+  live: 'result-wrapper-live',
+  notFound: 'result-wrapper-not-found',
+};
+
+const searchForm = document.querySelector(`.${searchStates.default}`);
+const searchInput = searchForm.querySelector('.search-block__input-string');
+const resultsWrapper = document.querySelector(`.${resultStates.none}`);
+const resultsList = document.querySelector('.result-list');
+
+const changeElementState = (element, state) => {
+  element.className = '';
+  element.classList.add(state);
+};
+
+const clearContainer = (container) => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+
+const render = (movieData) => {
+  const movie = document.createElement('movie-card');
+
+  movie.poster = movieData.poster;
+  movie.title = movieData.title;
+  movie.year = movieData.year;
+  movie.link = movieData.link;
+
+  return movie;
+};
+
+const search = async (searchTerm) => {
+  clearContainer(resultsList);
+
+  const { Search } = await fetch(
+    `http://www.omdbapi.com/?apikey=2fe365ef&type=movie&s=${searchTerm}`
+  ).then((r) => r.json());
+
+  const movies = Search.map((result) => render(mapMovie(result)));
+
+  const fragment = document.createDocumentFragment();
+
+  movies.forEach((movie) => fragment.appendChild(movie));
+  resultsList.appendChild(fragment);
+};
+
+const subscribeToSubmit = () => {
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!resultsWrapper.classList.contains(resultStates.live)) {
+      console.log('RESULT: none -> live');
+      changeElementState(resultsWrapper, resultStates.live);
+    }
+    search(searchInput.value);
+  });
+};
+
+searchInput.addEventListener('click', () => {
+  if (!searchForm.classList.contains(searchStates.active)) {
+    console.log('SEARCH: default -> active');
+    changeElementState(searchForm, searchStates.active);
+    subscribeToSubmit();
+  }
+});
